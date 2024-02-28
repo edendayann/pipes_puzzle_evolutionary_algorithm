9c391f1da1cf4559fd6161376a91c38dfe589139
edendayann <edayan333@gmail.com>
2024-02-28 23:23:50 +0200

    combine main and UI 28.2

--- UI.py
+++ UI.py
@@ -3,20 +3,22 @@ from tkinter import ttk
 
 
 class PipesPuzzleUI:
-    def __init__(self, master):
+    def __init__(self, master, main_function):
         self.master = master
+        self.main_function = main_function
         self.master.title("Pipes Puzzle Simulator")
 
         # Create the puzzle board
         self.canvas = tk.Canvas(self.master, width=700, height=500, borderwidth=2, relief="ridge")
         self.canvas.pack(side=tk.RIGHT, padx=10, pady=10)
 
-        # choose size frame
+        # board size frame
         self.frame_board_size = tk.Frame(self.master)
         self.frame_board_size.pack(padx=10, pady=10, anchor="w")
         self.label_board_size = tk.Label(self.frame_board_size, text="Choose board size:", anchor="w")
         self.label_board_size.pack(side=tk.LEFT, padx=5)
         self.value_board_size = ttk.Combobox(self.frame_board_size, values=["4", "5", "7"], width=5)
+        self.value_board_size.set("4")
         self.value_board_size.pack(side=tk.LEFT)
         self.value_board_size.bind("<<ComboboxSelected>>", self.select_board_size)
 
@@ -27,6 +29,7 @@ class PipesPuzzleUI:
         self.label_population_size.pack(side=tk.LEFT, padx=(0, 5))
         self.value_population_size = tk.Scale(self.frame_population_size, from_=50, to=500, orient=tk.HORIZONTAL,
                                               showvalue=True)
+        self.value_population_size.set(100)
         self.value_population_size.pack(side=tk.LEFT, padx=(0, 5))
 
         # elitism rate frame
@@ -36,6 +39,7 @@ class PipesPuzzleUI:
         self.label_elitism_rate.pack(side=tk.LEFT, padx=(0, 5))
         self.value_elitism_rate = tk.Scale(self.frame_elitism_rate, from_=0.0, to=1.0, orient=tk.HORIZONTAL,
                                            showvalue=True, resolution=0.01)
+        self.value_elitism_rate.set(0.05)
         self.value_elitism_rate.pack(side=tk.LEFT, padx=(0, 5))
 
         # max generation frame
@@ -45,6 +49,7 @@ class PipesPuzzleUI:
         self.label_max_generation.pack(side=tk.LEFT, padx=(0, 5))
         self.value_max_generation = tk.Scale(self.frame_max_generation, from_=0, to=500, orient=tk.HORIZONTAL,
                                              showvalue=True)
+        self.value_max_generation.set(50)
         self.value_max_generation.pack(side=tk.LEFT, padx=(0, 5))
 
         # moves frame
@@ -61,6 +66,7 @@ class PipesPuzzleUI:
         self.label_moves_range = tk.Label(self.frame_moves_range, text="Moves Range:", anchor="w")
         self.label_moves_range.pack_forget()  # Hide initially
         self.value_moves_range = tk.Scale(self.frame_moves_range, from_=0, to=50, orient=tk.HORIZONTAL, showvalue=True)
+        self.value_moves_range.set(4)
         self.value_moves_range.pack_forget()  # Hide initially
 
         # Submit button
@@ -85,28 +91,11 @@ class PipesPuzzleUI:
             self.value_moves_range.pack_forget()
 
     def handle_submit(self):
-        # Add logic to submit all values
-        value_1 = self.value_board_size.get()
-        value_2 = self.value_is_moves.get()
-        scale_value = self.value_moves_range.get()
-        print("Submitted values: ", value_1, value_2, scale_value)
-
-
-def main():
-    root = tk.Tk()
-    app = PipesPuzzleUI(root)
-    # # Get the screen width and height
-    # screen_width = root.winfo_screenwidth()
-    # screen_height = root.winfo_screenheight()
-    #
-    # # Calculate the position to center the window
-    # x = (screen_width - root.winfo_reqwidth()) /4
-    # y = (screen_height - root.winfo_reqheight())/4
-    #
-    # # Set the position of the window
-    # root.geometry("+%d+%d" % (x, y))
-    root.mainloop()
-
-
-if __name__ == "__main__":
-    main()
+        board_size = int(self.value_board_size.get())
+        is_moves = self.value_is_moves.get() == "Yes"
+        moves_range = self.value_moves_range.get()
+        population_size = self.value_population_size.get()
+        elitism_rate = self.value_elitism_rate.get()
+        max_generation = self.value_max_generation.get()
+        self.main_function(board_size=board_size, is_moves=is_moves, moves_range=moves_range,
+                           population_size=population_size, elitism_rate=elitism_rate, max_generation=max_generation)
--- main.py
+++ main.py
@@ -1,44 +1,31 @@
 import time
+import tkinter as tk
 
 from eckity.fitness.simple_fitness import SimpleFitness
 from eckity.genetic_operators.crossovers.vector_k_point_crossover import VectorKPointsCrossover
 from eckity.genetic_operators.mutations.vector_n_point_mutation import VectorNPointMutation
 from eckity.genetic_operators.selections.tournament_selection import TournamentSelection
 from eckity.termination_checkers.threshold_from_target_termination_checker import ThresholdFromTargetTerminationChecker
-
 from BoardCreator import BoardCreator
 from BoardEvaluator import BoardEvaluator
 from BoardEvolution import BoardEvolution
 from Boards import Boards
 from eckity.statistics.best_average_worst_statistics import BestAverageWorstStatistics
 from eckity.subpopulation import Subpopulation
-
 from BoardIndividual import BoardIndividual
+from UI import PipesPuzzleUI
 
 
-
-
-
-def main():
+def start_algorithm(board_size, is_moves, population_size, elitism_rate, max_generation, moves_range=4):
     start = time.time()
+    board = Boards(board_size)
 
-    size = int(input("Choose board size- 4/5/7: "))
-    board = Boards(size)
-    moves = input("Do you want to test moves- Y/N? ")
-    if moves == 'Y' or moves == 'y':
-        is_moves = True
-        moves_range = int(input("How many? "))
-    else:
-        is_moves = False
-        moves_range = 4
-    population_size = int(input("Enter population size: "))
-    elitism_rate = float(input("Enter elitism rate: "))
     # Initialize the evolutionary algorithm
     algo = BoardEvolution(
-        Subpopulation(creators=BoardCreator(size=size, moves_range=moves_range),
+        Subpopulation(creators=BoardCreator(size=board_size, moves_range=moves_range),
                       population_size=population_size,
                       # user-defined fitness evaluation method
-                      evaluator=BoardEvaluator(size=size, board_shapes=board.shapes,
+                      evaluator=BoardEvaluator(size=board_size, board_shapes=board.shapes,
                                                optimal_solution=board.optimal, is_moves=is_moves,
                                                moves_range=moves_range),
                       # maximization problem (fitness is sum of values), so higher fitness is better
@@ -55,8 +42,8 @@ def main():
                       ]
                       ),
         max_workers=4,
-        max_generation=50,
-        termination_checker=ThresholdFromTargetTerminationChecker(optimal=eval_optimal(size, board.shapes,
+        max_generation=max_generation,
+        termination_checker=ThresholdFromTargetTerminationChecker(optimal=eval_optimal(board_size, board.shapes,
                                                                                        board.optimal),
                                                                   threshold=0.001, higher_is_better=True),
         statistics=BestAverageWorstStatistics()
@@ -75,6 +62,12 @@ def eval_optimal(size, board_shapes, optimal_solution):
 
 def mut_val_getter(vec, idx):
     return vec.get_random_number_in_bounds(idx)
+
+
+def main():
+    root = tk.Tk()
+    app = PipesPuzzleUI(root, start_algorithm)
+    root.mainloop()
 
 
 # Press the green button in the gutter to run the script.
